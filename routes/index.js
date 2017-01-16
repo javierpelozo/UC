75ee4d3b88efc7e13c22106205eb440731495599
javierpelozo <javpelozo@gmail.com>
2017-01-15 21:44:21 -0300

    Other test1

--- routes/index.js
+++ routes/index.js
@@ -1,23 +1,3 @@
-/**
- * This file is where you define your application routes and controllers.
- *
- * Start by including the middleware you want to run for every request;
- * you can attach middleware to the pre('routes') and pre('render') events.
- *
- * For simplicity, the default setup for route controllers is for each to be
- * in its own file, and we import all the files in the /routes/views directory.
- *
- * Each of these files is a route controller, and is responsible for all the
- * processing that needs to happen for the route (e.g. loading data, handling
- * form submissions, rendering the view template, etc).
- *
- * Bind each route pattern your application should respond to in the function
- * that is exported from this module, following the examples below.
- *
- * See the Express application routing documentation for more information:
- * http://expressjs.com/api.html#app.VERB
- */
-
 var keystone = require('keystone');
 var middleware = require('./middleware');
 var importRoutes = keystone.importer(__dirname);
@@ -40,7 +20,6 @@ exports = module.exports = function (app) {
 	app.get('/eventos/:category?', routes.views.eventos);
 	app.get('/eventos/post/:post', routes.views.post);
 	app.get('/galeria', routes.views.galeria);
-	app.get('/gallery', routes.views.gallery);
 	app.get('/unidadpedagogica', routes.views.unidadpedagogica);
 	app.get('/identidad', routes.views.identidad);
 	app.all('/contacto', routes.views.contacto);
